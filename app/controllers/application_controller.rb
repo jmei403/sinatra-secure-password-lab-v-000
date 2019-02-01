e0b7c72ccc5f3f89880efcb31b2751bf1213fd13
Jason Mei <jason.mei403@gmail.com>
2019-02-01 01:00:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     @user = User.new(username: params[:username], password: params[:password])
 
-    if @user.save && !@user[:username].nil?
+    if @user.save && @user[:username] != ""
       binding.pry
       redirect "/login"
     else
